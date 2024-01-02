0889afc04606bde6a60e745a645a79d728340793
VirLg <19virlg@gmail.com>
2024-01-02 18:23:08 +0200

    add

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/react-create-app">
+      <BrowserRouter basename="/">
         <App />
       </BrowserRouter>
     </Provider>
